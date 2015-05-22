1042e4e693adf5d42283c4a84b36b8c8c8bc49cd
Vlad Filiucov <filiucov@mail.ru>
2015-05-22 14:38:22 +0300

    Add user method for creating todo list tutorial.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -52,7 +52,7 @@ describe User do
 	it "downcases an email before saving" do
 			user = User.new(valid_attributes)
 			user.email = "JORAKORNEV@MAIL.RU"
-			expect(user.save).to be_true
+			expect(user.save).to be true
 			expect(user.email).to eq("jorakornev@mail.ru")
 		end
 	end
@@ -68,4 +68,30 @@ describe User do
 			user.generate_password_reset_token!
 		end
 	end
+
+  describe "#create_default_lists" do
+    let(:user) { create(:user) }
+    it "creates a todo list" do
+      expect{ user.create_default_lists }.to change{ user.todo_lists.size }.by(1)
+    end
+
+    it "does not create the same todo list twice" do
+      expect{ user.create_default_lists }.to change{ user.todo_lists.size }.by(1)
+      expect{ user.create_default_lists }.to change{ user.todo_lists.size }.by(0)      
+    end
+
+    it "creates todo items" do
+      expect { user.create_default_lists }.to change{ TodoItem.count }.by(7)
+    end
+
+    it "creates todo items only once" do
+      expect { user.create_default_lists }.to change{ TodoItem.count }.by(7)
+      expect { user.create_default_lists }.to change{ TodoItem.count }.by(0)
+    end    
+  end
+
+
+
+
+
 end
